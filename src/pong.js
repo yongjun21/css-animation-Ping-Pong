6b9a41b0a318e95833f0cc90653fe95b2355e56c
Yong Jun <yjthong.2009@smu.edu.sg>
2015-11-14 00:31:15 +0800

    Added prefix, Implement responsive, Modified package.json

--- src/pong.js
+++ src/pong.js
@@ -1,16 +1,18 @@
 var ball = document.querySelector('#ball');
 var playerTurn = true;
+var tbLength = window.matchMedia(
+  '(min-width: 1200px)').matches ? '1200px' : '700px';
 
 var listener = function (event) {
   var randomSpeed = Math.round(1000 + 300 * Math.random());
   ball.style.transitionDuration = randomSpeed + 'ms';
   if (playerTurn) ball.style.left = '0';
-  else ball.style.left = '1200px';
+  else ball.style.left = tbLength;
   playerTurn = !playerTurn;
 };
 
 ball.addEventListener('transitionend', listener);
 window.setTimeout(function () {
-  ball.style.left = '1200px';
+  ball.style.left = tbLength;
   ball.style.visibility = 'visible';
 }, 400);
