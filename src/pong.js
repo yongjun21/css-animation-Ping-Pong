37bb2a79f2986b4a00472d1c4e2e369e03c87e85
Yong Jun <yjthong.2009@smu.edu.sg>
2015-11-27 10:11:30 +0800

    Refactored code

--- src/pong.js
+++ src/pong.js
@@ -1,18 +1,13 @@
 var ball = document.querySelector('#ball');
-var playerTurn = true;
-var tbLength = window.matchMedia(
-  '(min-width: 1200px)').matches ? '1200px' : '700px';
 
 var listener = function (event) {
   var randomSpeed = Math.round(1000 + 300 * Math.random());
   ball.style.transitionDuration = randomSpeed + 'ms';
-  if (playerTurn) ball.style.left = '0';
-  else ball.style.left = tbLength;
-  playerTurn = !playerTurn;
+  ball.classList.toggle('playerSide');
 };
 
 ball.addEventListener('transitionend', listener);
 window.setTimeout(function () {
-  ball.style.left = tbLength;
+  ball.classList.toggle('playerSide');
   ball.style.visibility = 'visible';
 }, 400);
